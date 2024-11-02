b1febb43db2a6868609871d696f0a361f3cf5a7b
Sandy <aryadikawidodo0@gmail.com>
2024-11-02 21:31:18 +0700

    revisi handler & routes

--- src/handler.js
+++ src/handler.js
@@ -1,40 +1,45 @@
-const { nanoid } = require ('nanoid');
+const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
-    
     const id = nanoid(16);
-    const createAt = new Date().toISOString();
-    const updateAt = createAt;
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
 
     const newNote = {
-        title, tags, body, id, createAt, updateAt,
+        id,
+        title,
+        tags,
+        body,
+        createdAt,
+        updatedAt,
     };
 
     notes.push(newNote);
 
+    // check if notes successuffly added
     const isSuccess = notes.filter((note) => note.id === id).length > 0;
+    console.log(isSuccess);
 
     if (isSuccess) {
-        const response = h.response({
-            status: 'success',
-            message: 'Catatan berhasil ditambahkan',
-            data: {
-                noteId : id,
-            },
-        });
-        response.code(201);
-        return response;
+        return h
+            .response({
+                status: 'success',
+                message: 'Catatan berhasil ditambahkan',
+                data: {
+                    noteId: id,
+                },
+            })
+            .code(201);
     }
-
-    const response = h.response({
-        status: 'fail',
-        message: 'Catatan gagal ditambahkan',
-    });
-    response.code(500);
-    return response;
-}
+    return h
+        .response({
+            status: 'fail',
+            message: 'Catatan gagal ditambahkan',
+        })
+        .code(500);
+};
 
 const getAllNotesHandler = () => ({
     status: 'success',
@@ -45,80 +50,88 @@ const getAllNotesHandler = () => ({
 
 const getNoteByIdHandler = (request, h) => {
     const { id } = request.params;
-
     const note = notes.filter((n) => n.id === id)[0];
+    // [0] bearti notes pertama yang ditemukan, kalo bae id duplokat
+    console.log(note);
 
     if (note !== undefined) {
-        return {
-            status: 'success',
-            data: {
-                note,
-            },
-        };
+        return h
+            .response({
+                status: 'success',
+                data: {
+                    note,
+                },
+            })
+            .code(200);
     }
-
-    const response = h.response({
-        status: 'fail',
-        message: 'Catatan tidak ditemukan!',
-    });
-    response.code(404);
-    return response;
+    return h
+        .response({
+            status: 'fail',
+            message: 'Catatan tidak ditemukan',
+        })
+        .code(404);
 };
 
 const editNoteByIdHandler = (request, h) => {
     const { id } = request.params;
-
     const { title, tags, body } = request.payload;
-    const updateAt = new Date().toISOString();
+    const updatedAt = new Date().toISOString();
+    const index = notes.findIndex((note) => note.id === id);
 
-    const index = notes.findIndex((note) => node.id === id);
-
-    if (index !== 1){
+    if (index !== -1) {
         notes[index] = {
             ...notes[index],
-            title,
-            tags,
-            body,
-            updateAt,
+            title: title || notes[index].title,
+            tags: tags || notes[index].tags,
+            body: body || notes[index].body,
+            updatedAt,
         };
 
-        const response = h.response({
-            status: 'success',
-            message: 'Catatan berhasil diperbarui',
-        });
-        response.code(200);
-        return response;
+        return h
+            .response({
+                status: 'success',
+                message: 'Catatan berhasil diperbarui',
+            })
+            .code(200);
     }
-    
-    const response = h.response({
-        status: 'fail',
-        message: 'Gagal memperbarui catatan. Id tidak ditemukan',
-    });
-    response.code(404);
-    return response;
+
+    return h
+        .response({
+            status: 'fail',
+            message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+        })
+        .code(404);
 };
 
-const deleteNoteByHandler = (request, h) => {
+const deleteNoteByIdHandler = (request, h) => {
     const { id } = request.params;
-
     const index = notes.findIndex((note) => note.id === id);
 
     if (index !== -1) {
         notes.splice(index, 1);
-        const response = h.response({
-            status: 'success',
-            message: 'Catatan berhasil di perbarui',
-        });
-        response.code(200);
-        return response
+        // splice(index, 1) berarti menghapus 1 element dari array
+        // dari index yang ditemukan
+
+        return h
+            .response({
+                status: 'success',
+                message: 'Catatan berhasil dihapus',
+            })
+            .code(200);
     }
 
-    const response = h.response({
-        status: 'fail',
-        message: 'Catatan gagal dihapus. Id tidak ditemukan',
-    });
-    response.code(404);
-    return response;
+    return h
+        .response({
+            status: 'fail',
+            message: 'Catatan gagal dihapus. Id tidak ditemukan',
+        })
+        .code(404);
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByHandler };
+module.exports = {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+};
--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-  const Hapi = require('@hapi/hapi');
-  const routes = require('./routes');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
-  const init = async () => {
-    const server = Hapi.server({
-      port: 5000,
-      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-      routes: {
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
         cors: {
-          origin: ['*'],
+            origin: ['*'],
         },
-      },
-    });
+        security: false,
+    },
+  });
 
-    server.route(routes);
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
-  };
+  server.route(routes);
 
-  init();
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
